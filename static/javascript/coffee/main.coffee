7f617bbad1935c9888519bfcf21fb79265ef9c98
Matt Whitesides <mbwxd4@mail.missouri.edu>
2014-09-04 11:04:52 -0500

    added sublime tutorial

--- static/javascript/coffee/main.coffee
+++ static/javascript/coffee/main.coffee
@@ -2,4 +2,7 @@ $(document).ready ->
   $("#notepadTutorial").click ->
     $("#sftpTutorialDiv").load "notepadTutorial.html"
   return
+  $("#sublimeTutorial").click ->
+    $("#sftpTutorialDiv").load "sublimeTutorial.html"
+  return  
 return
--- static/javascript/js/main.js
+++ static/javascript/js/main.js
@@ -3,6 +3,10 @@
     $("#notepadTutorial").click(function() {
       return $("#sftpTutorialDiv").load("notepadTutorial.html");
     });
+    return;
+    $("#sublimeTutorial").click(function() {
+      return $("#sftpTutorialDiv").load("sublimeTutorial.html");
+    });
   });
 
   return;
